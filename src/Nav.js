c4b408817ccde3c97c9fb554ea2717ebcf7ac958
Khallan <khallanjamal1@gmail.com>
2022-10-26 15:38:11 -0400

    update

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './app.css';
+
+function Nav() {
+  return (
+    <nav>
+        <h3>Khallan's Website</h3>
+        <ul className='nav-links'>
+            <li>Home</li>
+            <li>Internship Logs</li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Nav;
---
+++ src/internshiplogs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+function internshiplogs() {
+  return (
+    <div>
+      <h1>Internship Logs</h1>
+    </div>
+  );
+}
+
+export default internshiplogs;
