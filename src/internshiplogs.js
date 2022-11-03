762fed2bb06ee07fb5f1cda8050b265096a1613e
Khallan <khallanjamal1@gmail.com>
2022-11-03 16:02:42 -0400

    Nov3

--- src/internshiplogs.js
+++ src/internshiplogs.js
@@ -3,51 +3,140 @@ function Internshiplogs() {
   return (
     <div>
       <h1>Internship Logs</h1>
-      
-      <br />
-      <b>October 27</b>
-      <p>
-      Today, I finally hosted my react application. I then moved my nav bar over to a seperate file from the app.js to make it less confusing in Explorer tab.
-      I have made the nav bar have a function. When I click onto the internship logs it takes you to this page, and when you click onto the home page it takes you back to a 
-      blank page. This is going to be temporarily blank. I am going to add a quick about me section on the first page so it is not empty. When you flip back and forth from
-      the tabs the nav bar stays while underneath renders, thus saving time and energy. I also learned how to refresh a deployed react app. All in all a very successful day in
-      my opinion.
-      <br />
-      <br />
-      <b>October 28</b>
-      <br />
-      Today, we had a meeting about Trello, and what we are going to be doing on November 2. I begun the day by creating a very short list about what I am going ot say, this took about
-      5 minutes of the time. I then did a lot of research on branches and how they work. I have created branches in myapp terminal and I think I have a good understanding of 
-      what they are and how they work. I also learned how pull requests are and how they work. With branches and pull requests you can work on the same project with other people. 
-      Each person creates there own branch and works on it by themselves then they merge their branches to the master and then all there changes are add to the file. This is what I spent 
-      the vast majority of my time doing. I did not do what you mentioned in dicord last night, I will do that at the beginning on monday.
-     </p>
-     <br /> 
-     <br />
-     <b>October 31</b>
-     <br />
-     Today, I tried to fix the bugs in my code that I didn't realize yesterday, some of the bugs I attempeted to fix was the Navigation Bar hover feature. 
-     When you hoverd your cursor over the buttons both the tabs would highlight, I don't know why this was happening because on my other code it works perfectly
-     fine. Another problem that occured today that is farmillar to me is the underline under the Navigation tasb will not remove, with text decorations, I have 
-     decided to leave it for now. I then wanted to get started with my About Me page. I started it off but then I wanted to be creative and have it where you scroll
-     to find out stuff. I just started it now so I do not have much. 
-    <br />
-    <br />
-    <b>November 1</b>
-    <br />
-    At the beginning, Fabien and I worked on our presentation for tomorrow morning. We have written down and memorized so dot point for tomorrow. I then
-    begun my editing my code. I seemingly did not do anything to my code but it had a bug I could not find. I spent very long on this and I had to because I 
-    could not progress without it. Google did not help me because I did everything correct I just acceidentally deleted a closeing bracket from my function.
-    I had to get a second eye on this problem to see the solution. After I solved this problem with help. I then started to learn about API's, 
-    I also read up a little on GET requests. Then just like that the day was over.
-    <br />
-    <br />
-    <b>November 2</b>
-    <br />
-    Today, I began with reasearching API, I now have an understanding of the concept. Then we talked about a new project where you want me to create a new
-    tab on North P&D website. I began this task by downloading your code onto my computer and going through your code. I then created a branch named "our-expertise-branch".
-    I then tired to run the local host but got an error message and googled how to fix it. I tried many things but none of them worked, at least I think
-    I will try again tomorrow. I then put a pause on "Our expertise" project and then worked on fixing my website "Error 404" message.
+        <b>October 6th</b>
+        <br /> 
+        Today, I learned how to begin a website with HTML. 
+        I also figured out GitHub and Git and I am using VS editor to write this. 
+        I have learned how to add a picture and put in a hyperlink.
+        <br />
+        <br />
+        <b>October 11</b>
+        <br />
+        Today I continued on my website using HTML and started to use CSS to create the header. 
+        Right now I have “Home” and “Shop” but those are temporary. I will change it when I get to putting something on the other end. 
+        Also created a very simple chart at the bottom, and created a list above it. Just so I can get comfortable with HTML. 
+        I have done some research in Github pages and Github to get an understanding of the programs. 
+        I also went through some shortcuts for VS code and regular shortcuts that I don't know. Tomorrow I am going to hopefully publish my website.
+        <br />
+        <br />
+        <b>October 12</b>
+        <br />
+        Today, I have published this website, this took most of the time. I am unsure on how to update the website so I will learn that tomorrow. 
+        The link to the old version of my website in the chat for you take a look at.
+         I also created working tabs in the navagation bar. Fabien helped me with creating 
+        it and making it work. Tomorrow I will make the navagation bar easier on the eyes and begin learing JavaScript.
+        <br />
+        <br />
+        <b>October 13</b>
+        <br />
+        Today, we had a standup meeting with JP, Fabien and I about how servers work. 
+        After the meeting I tried to make my website more appealing and remove “/webpage” from the URL. 
+        I unfortunately could not do that because I ran into errors that I don't know how to fix nor does Fabien. 
+        When trying to fix these problems we googled it but it did not help us. I decided to restart my website. 
+        So I took down my website and deleted the repository. 
+        I copied my code into the new repository and updated and published my website.
+        <br />
+        <br />
+        <b>October 14</b>
+        <br />
+        Today, I learned about Git Bash and how to post/update a website without external help. I also created a CSS sheet to make my website look nicer.
+        I am still having some trouble removing the underline and blue colour from the tabs at the top of the page.
+        <br />
+        <br />
+        <b>October 17</b>
+        <br />
+        Today, I created a footer for my webpage and added a picture of me and a short description about me.
+        <br />
+        <br />
+        <b>October 19</b>
+        <br />
+        Today, I made my website look nicer than before. I finally found out how to remove the underline from the tabs, it also removed the underline from the google
+        hyperlink, I will change that tomorrow. I also added a little description about me in the main page and removed the header from the footer.
+        <br />
+        <br />
+        <b>Ocotober 20</b>
+        <br />
+        I spent the day learning react and trying to convert my HTML code over to react JS. I was unsuccessful in doing so. I added a temporary like button
+        just so I can get a feel of what JS is about, I will remove that soon. I then downloaded a node to my computer to use in the terminal for react. I am 
+        unsure what that does exactly, but I will figure it out tomorrow. React seems very complex but I am eager to learn it. Over the weekend if I am successful 
+        in converting my code to JS I want to make my website look better.  
+        <br />
+        <br />
+        <b>October 21</b>
+        <br />
+        Today I finally got a React folder to work on my computer. I spent most of the time figuring it out. I have just begun converting my code to JS but I don't
+        have anyhting done code wise. I want to catch up to Fabien a little bit, so I am going to convert some of my code over to js on the weekend when I have 
+        the time. I do not have much exprience with React as I just started with the program. 
+        <br />
+        <br />
+        <b>Ocotober 24</b>
+        <br />
+        Although I did not do much I learned a lot of JavaScript. I have begun my navbar on my new react website. I tired to also add a menu icon when the screen
+        is too small. I tired to do this as I was making the navbar, so they are both half done. Posted below is what I have so far in React JS. This is only going to
+        here for the time being as I shift over to my new website <br />
+        <br />
+        <b>Ocotober 25</b>
+        <br />
+        Today, I created a header with buttons on my new react application. I could not get these buttons to do anything, I will try again tomorrow. The 
+        buttons I created on the navbar go to my home page and to my internship logs. I have also created the CSS and made it have a gradient background. 
+        Also when you hover over the buttons a box around the word appears slowly and fades away slowly when you put and take your cursor off the button.
+        I have also tried to publish my react app but I am not sure if it has uploaded or not. I don't think it has I must of done somthing wrong. I think
+        that I have learned alot about React JavaScript, and feel that I was very productive today. From yesterday, I was not able to get the menu icon 
+        to appear or work when my website is used on a phone. I will continue that problem later when I have my website working like this one here. Below I put
+        my React App header so you can see my progress on the other application. <br />
+        <br />
+        <b>Ocotober 26</b>
+        <br />
+        Today I continued with my work from the otherday. I spent most of my day tring to get navagation tabs to go to different parts of my website.
+        I could not make the tabs work, the tutorial must of been out dated thus my code would not work. I also re-did what I did yesterday because the tutorial
+        that I was following yesterday was more complex then is needed to be so I rewrote my code differently which did not take much time at all. I then tried to
+        publish my website. I did manage to get a website up and running but it is not what I created. The website that I published looks like documentation on how 
+        to properly publish a website, I will see if that helps tomorrow. I am having more trouble than I thought I was going to when I began React and JavaScript
+        <br />
+        <br />
+        <b>October 27</b>
+        <br />
+        Today, I finally hosted my react application. I then moved my nav bar over to a seperate file from the app.js to make it less confusing in Explorer tab.
+        I have made the nav bar have a function. When I click onto the internship logs it takes you to this page, and when you click onto the home page it takes you back to a 
+        blank page. This is going to be temporarily blank. I am going to add a quick about me section on the first page so it is not empty. When you flip back and forth from
+        the tabs the nav bar stays while underneath renders, thus saving time and energy. I also learned how to refresh a deployed react app. All in all a very successful day in
+        my opinion.
+        <br />
+        <br />
+        <b>October 28</b>
+        <br />
+        Today, we had a meeting about Trello, and what we are going to be doing on November 2. I begun the day by creating a very short list about what I am going ot say, this took about
+        5 minutes of the time. I then did a lot of research on branches and how they work. I have created branches in myapp terminal and I think I have a good understanding of 
+        what they are and how they work. I also learned how pull requests are and how they work. With branches and pull requests you can work on the same project with other people. 
+        Each person creates there own branch and works on it by themselves then they merge their branches to the master and then all there changes are add to the file. This is what I spent 
+        the vast majority of my time doing. I did not do what you mentioned in dicord last night, I will do that at the beginning on monday.
+        <br />
+        <br />
+        <b>October 31</b>
+        <br />
+        Today, I tried to fix the bugs in my code that I didn't realize yesterday, some of the bugs I attempeted to fix was the Navigation Bar hover feature. 
+        When you hoverd your cursor over the buttons both the tabs would highlight, I don't know why this was happening because on my other code it works perfectly
+        fine. Another problem that occured today that is farmillar to me is the underline under the Navigation tasb will not remove, with text decorations, I have 
+        decided to leave it for now. I then wanted to get started with my About Me page. I started it off but then I wanted to be creative and have it where you scroll
+        to find out stuff. I just started it now so I do not have much. 
+        <br />
+        <h2>November</h2>
+        <b>November 1</b>
+        <br />
+        At the beginning, Fabien and I worked on our presentation for tomorrow morning. We have written down and memorized so dot point for tomorrow. I then
+        begun my editing my code. I seemingly did not do anything to my code but it had a bug I could not find. I spent very long on this and I had to because I 
+        could not progress without it. Google did not help me because I did everything correct I just acceidentally deleted a closeing bracket from my function.
+        I had to get a second eye on this problem to see the solution. After I solved this problem with help. I then started to learn about API's, 
+        I also read up a little on GET requests. Then just like that the day was over.
+        <br />
+        <br />
+        <b>November 3</b>
+        <br />
+        Today, I began with reasearching API, I now have an understanding of the concept. Then we talked about a new project where you want me to create a new
+        tab on North P&D website. I began this task by downloading your code onto my computer and going through your code. I then created a branch named "our-expertise-branch".
+        I then tired to run the local host but got an error message and googled how to fix it. I tried many things but none of them worked, at least I think
+        I will try again tomorrow. I then put a pause on "Our expertise" project and then worked on fixing my website "Error 404" message. Once I fixed that issue
+        I then moved over all my old Internship logs to my this website. 
     </div>
   );
 }
